38dfe8003d864a2ef7153e1224460ae1bb382677
Paul Savchuk <nekrjkeee@gmail.com>
2023-05-05 10:55:26 +0300

    refactor method get_images to static

--- player.py
+++ player.py
@@ -16,7 +16,11 @@ class Player:
         self.health_battery = PLAYER_MAX_HEALTH // 2
 
     def recover_health(self):
-        if self.check_health_recovery_delay() and self.health < PLAYER_MAX_HEALTH and self.health_battery:
+        if (
+            self.check_health_recovery_delay()
+            and self.health < PLAYER_MAX_HEALTH
+            and self.health_battery
+        ):
             self.health += 1
             self.health_battery -= 1
 
@@ -44,7 +48,11 @@ class Player:
 
         if event.type == pg.MOUSEBUTTONDOWN or (keys[pg.K_f] and keys[pg.K_LSHIFT]):
             try:
-                if event.button == 1 and not self.shot and not self.game.weapon.reloading:
+                if (
+                    event.button == 1
+                    and not self.shot
+                    and not self.game.weapon.reloading
+                ):
                     self.game.sound.shotgun.play()
                     self.shot = True
                     self.game.weapon.reloading = True
@@ -95,10 +103,17 @@ class Player:
             self.y += dy
 
     def draw(self):
-        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
-                    (self.x * 100 + WIDTH * math.cos(self.angle),
-                     self.y * 100 + WIDTH * math. sin(self.angle)), 2)
-        pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
+        pg.draw.line(
+            self.game.screen,
+            "yellow",
+            (self.x * 100, self.y * 100),
+            (
+                self.x * 100 + WIDTH * math.cos(self.angle),
+                self.y * 100 + WIDTH * math.sin(self.angle),
+            ),
+            2,
+        )
+        pg.draw.circle(self.game.screen, "green", (self.x * 100, self.y * 100), 15)
 
     def mouse_control(self):
         mx, my = pg.mouse.get_pos()
--- sound.py
+++ sound.py
@@ -5,17 +5,17 @@ class Sound:
     def __init__(self, game):
         self.game = game
         pg.mixer.init()
-        self.path = 'resources/sound/'
-        self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
-        self.npc_pain = pg.mixer.Sound(self.path + 'npc_pain.wav')
-        self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
+        self.path = "resources/sound/"
+        self.shotgun = pg.mixer.Sound(self.path + "shotgun.wav")
+        self.npc_pain = pg.mixer.Sound(self.path + "npc_pain.wav")
+        self.npc_death = pg.mixer.Sound(self.path + "npc_death.wav")
         # caco
-        self.npc_caco_death = pg.mixer.Sound(self.path + 'npc_caco_death.mp3')
-        self.npc_caco_attack = pg.mixer.Sound(self.path + 'npc_caco_attack.mp3')
-        self.npc_caco_pain = pg.mixer.Sound(self.path + 'npc_caco_pain.mp3')
+        self.npc_caco_death = pg.mixer.Sound(self.path + "npc_caco_death.mp3")
+        self.npc_caco_attack = pg.mixer.Sound(self.path + "npc_caco_attack.mp3")
+        self.npc_caco_pain = pg.mixer.Sound(self.path + "npc_caco_pain.mp3")
 
-        self.npc_shot = pg.mixer.Sound(self.path + 'npc_attack.wav')
+        self.npc_shot = pg.mixer.Sound(self.path + "npc_attack.wav")
         self.npc_shot.set_volume(0.2)
-        self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
-        self.theme = pg.mixer.music.load(self.path + 'theme.mp3')
+        self.player_pain = pg.mixer.Sound(self.path + "player_pain.wav")
+        self.theme = pg.mixer.music.load(self.path + "theme.mp3")
         pg.mixer.music.set_volume(0.4)
